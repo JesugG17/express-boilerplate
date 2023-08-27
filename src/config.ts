ceedcfdf6eb21e30df2c176eaf863c9da7ab6529
Jesus Gastelum <gastelumtec21@gmail.com>
2023-08-26 23:36:16 -0700

    make structure folder to following clean architecture

--- src/config.ts
+++ src/config.ts
@@ -4,19 +4,11 @@ import dotenv from "dotenv";
 // Parsing the env file.
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-// Interface to load env variables
-// Note these variables can possibly be undefined
-// as someone could skip these varibales or not setup a .env file at all
 
 interface ENV {
-  // SECRETKEY: string | undefined;
-  // your enviroment variables declared like the commented line above
 }
 
-interface Config {
-  // SECRETKEY: string;
-  // Again your enviroments variables declared like the commented line above
-}
+interface Config {}
 
 // Loading process.env as ENV interface
 
